e04ff5e8762d49a6aa0cef53f9c0b2e5a15bd484
lujun.chen <lujun.chen@msxf.com>
2018-09-29 13:33:10 +0800

    insert mysql

---
+++ src/main/java/com/itclj/mapper/SysRoleMapper.java
@@ -0,0 +1,30 @@
+package com.itclj.mapper;
+
+import com.itclj.model.SysRole;
+import com.itclj.model.SysRoleExample;
+import java.util.List;
+import org.apache.ibatis.annotations.Param;
+
+public interface SysRoleMapper {
+    long countByExample(SysRoleExample example);
+
+    int deleteByExample(SysRoleExample example);
+
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(SysRole record);
+
+    int insertSelective(SysRole record);
+
+    List<SysRole> selectByExample(SysRoleExample example);
+
+    SysRole selectByPrimaryKey(Integer id);
+
+    int updateByExampleSelective(@Param("record") SysRole record, @Param("example") SysRoleExample example);
+
+    int updateByExample(@Param("record") SysRole record, @Param("example") SysRoleExample example);
+
+    int updateByPrimaryKeySelective(SysRole record);
+
+    int updateByPrimaryKey(SysRole record);
+}
---
+++ src/main/java/com/itclj/model/SysRole.java
@@ -0,0 +1,45 @@
+package com.itclj.model;
+
+import java.util.Date;
+
+public class SysRole {
+    private Integer id;
+
+    private String code;
+
+    private String name;
+
+    private Date createTime;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Date getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(Date createTime) {
+        this.createTime = createTime;
+    }
+}
